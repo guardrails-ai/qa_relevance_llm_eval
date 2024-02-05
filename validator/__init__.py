07b401078f27eef8d7230c9d3a9921b9209ff3c0
Safeer Mohiuddin <safeer@guardrailsai.com>
2024-02-05 08:43:35 +0000

    uploading qa_relevance_llm_eval validator

--- validator/__init__.py
+++ validator/__init__.py
@@ -1,3 +1,3 @@
-from .main import RegexMatch
+from .main import QARelevanceLLMEval
 
-__all__ = ["RegexMatch"]
+__all__ = ["QARelevanceLLMEval"]
--- validator/main.py
+++ validator/main.py
@@ -1,9 +1,7 @@
-import re
-import string
-from typing import Any, Callable, Dict, Optional
+import inspect
+from typing import Any, Callable, Dict, Optional, cast
 
-import rstr
-
+from guardrails.utils.openai_utils import get_static_openai_chat_create_func
 from guardrails.validator_base import (
     FailResult,
     PassResult,
@@ -13,59 +11,92 @@ from guardrails.validator_base import (
 )
 
 
-@register_validator(name="guardrails/regex_match", data_type="string")
-class RegexMatch(Validator):
-    """Validates that a value matches a regular expression.
+@register_validator(name="guardrails/qa_relevance_llm_eval", data_type="string")
+class QARelevanceLLMEval(Validator):
+    """Validates that an answer is relevant to the question asked by asking the
+    LLM to self evaluate.
 
     **Key Properties**
 
-    | Property                      | Description                       |
-    | ----------------------------- | --------------------------------- |
-    | Name for `format` attribute   | `regex_match`                     |
-    | Supported data types          | `string`                          |
-    | Programmatic fix              | Generate a string that matches the regular expression |
+    | Property                      | Description                         |
+    | ----------------------------- | ----------------------------------- |
+    | Name for `format` attribute   | `qa-relevance-llm-eval`             |
+    | Supported data types          | `string`                            |
+    | Programmatic fix              | None                                |
 
-    Args:
-        regex: Str regex pattern
-        match_type: Str in {"search", "fullmatch"} for a regex search or full-match option
-    """  # noqa
+    Other parameters: Metadata
+        question (str): The original question the llm was given to answer.
+    """
+
+    required_metadata_keys = ["question"]
 
     def __init__(
         self,
-        regex: str,
-        match_type: Optional[str] = None,
+        llm_callable: Optional[Callable] = None,
         on_fail: Optional[Callable] = None,
+        **kwargs,
     ):
-        # todo -> something forces this to be passed as kwargs and therefore xml-ized.
-        # match_types = ["fullmatch", "search"]
+        super().__init__(on_fail, llm_callable=llm_callable, **kwargs)
 
-        if match_type is None:
-            match_type = "fullmatch"
-        assert match_type in [
-            "fullmatch",
-            "search",
-        ], 'match_type must be in ["fullmatch", "search"]'
+        if llm_callable is not None and inspect.iscoroutinefunction(llm_callable):
+            raise ValueError(
+                "QARelevanceLLMEval validator does not support async LLM callables."
+            )
 
-        super().__init__(on_fail=on_fail, match_type=match_type, regex=regex)
-        self._regex = regex
-        self._match_type = match_type
+        self.llm_callable = (
+            llm_callable if llm_callable else get_static_openai_chat_create_func()
+        )
+
+    def _selfeval(self, question: str, answer: str) -> Dict:
+        from guardrails import Guard
+
+        spec = """
+<rail version="0.1">
+<output>
+    <bool name="relevant" />
+</output>
+
+<prompt>
+Is the answer below relevant to the question asked?
+Question: {question}
+Answer: {answer}
+
+Relevant (as a JSON with a single boolean key, "relevant"):\
+</prompt>
+</rail>
+    """.format(
+            question=question,
+            answer=answer,
+        )
+        guard = Guard[Dict].from_rail_string(spec)
+
+        response = guard(
+            self.llm_callable,  # type: ignore
+            max_tokens=10,
+            temperature=0.1,
+        )
+        validated_output = cast(Dict, response.validated_output)
+        return validated_output
 
     def validate(self, value: Any, metadata: Dict) -> ValidationResult:
-        p = re.compile(self._regex)
-        """Validates that value matches the provided regular expression."""
-        # Pad matching string on either side for fix
-        # example if we are performing a regex search
-        str_padding = (
-            "" if self._match_type == "fullmatch" else rstr.rstr(string.ascii_lowercase)
-        )
-        self._fix_str = str_padding + rstr.xeger(self._regex) + str_padding
-
-        if not getattr(p, self._match_type)(value):
-            return FailResult(
-                error_message=f"Result must match {self._regex}",
-                fix_value=self._fix_str,
+        if "question" not in metadata:
+            raise RuntimeError(
+                "qa-relevance-llm-eval validator expects " "`question` key in metadata"
             )
-        return PassResult()
+
+        question = metadata["question"]
+
+        self_evaluation: Dict = self._selfeval(question, value)
+        relevant = self_evaluation["relevant"]
+        if relevant:
+            return PassResult()
+
+        fixed_answer = "No relevant answer found."
+        return FailResult(
+            error_message=f"The answer {value} is not relevant "
+            f"to the question {question}.",
+            fix_value=fixed_answer,
+        )
 
     def to_prompt(self, with_keywords: bool = True) -> str:
-        return "results should match " + self._regex
+        return ""
